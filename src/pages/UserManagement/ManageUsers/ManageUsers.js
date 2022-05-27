f0fb5e8165d175a4548072424c4da5f90806a23b
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2022-05-27 23:08:40 +0000

    imported one of the tables to check but going to sleep now, lol (would continue tomorrow)

--- src/pages/UserManagement/ManageUsers/ManageUsers.js
+++ src/pages/UserManagement/ManageUsers/ManageUsers.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./ManageUsers.scss";
+import ManageUsersTable from "./ManageUsersTable";
 
 const ManageUsers = () => {
-  return <div>ManageUsers</div>;
+  return (<div>ManageUsers
+      <ManageUsersTable />
+  </div>);
 };
 
 export default ManageUsers;
